4bccae9e35788a049b6bc259245b3406a157bc16
madrimas <zapiorkowski@gmail.com>
2020-10-17 12:28:43 +0200

    traveling salesman problem algorithm (prepared for searching hamiltonian cycle for Good Lood spots in Cracow)

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+	public static void main(String[] args) {
+		TravellingSalesmanProblem travellingSalesmanProblem = new TravellingSalesmanProblem();
+		travellingSalesmanProblem.start();
+	}
+}
